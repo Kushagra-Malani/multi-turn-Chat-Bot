08101ec0ec2694c353a36317463fcd7805e8167a
Kushagra <155226608+Kushagra-Malani@users.noreply.github.com>
2024-09-01 05:49:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [ans, setAns] = useState("")
+  const [value, setValue] = useState("")
+  const [error, setError] = useState("")
+  const [chatHistory, setChatHistory] = useState([])
+
+  const surpriseOptions = [
+    "who won the latest noble peace prize ",
+    "where does pizza come from",
+    "how to make a sandwitch"
+  ]
+
+  const surprise = () => {
+    const randomValue = surpriseOptions[Math.floor(Math.random()*surpriseOptions.length)]
+    setValue(randomValue)
+  }
+
+  const getResponse = async() => {
+    if(!value){
+      setError("Error: Please enter a question")
+      return
+    }
+    try {
+      const options = {
+        method: 'POST',
+        body: JSON.stringify({
+          history: chatHistory,
+          message: value
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+      const response = await fetch('http://localhost:8000/gemini', options)
+      const data = await response.text()
+      setAns(data)
+      console.log(data);
+      // setChatHistory([{
+      //   role: "user",
+      //   parts: value
+      // },
+      // {
+      //   role: "model",
+      //   parts: data
+      // }
+      // ])
+      setValue("")
+    } catch (error) {
+      console.log(chatHistory);
+      console.log(error);
+      setError("Something went wrong! Please try again later")
+    }
+  }
+
+  const clear = () => {
+    setValue("");
+    setError("")
+    setChatHistory([])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <section className="search-section">
+        <p>What do you want to know?
+          <button className="surprise" onClick={surprise} disabled={!chatHistory}>Surprise me</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="input-container">
+          <input
+            value={value}
+            placeholder="When is Christmas...?"
+            onChange={(e) => setValue(e.target.value)}
+          />
+          {!error && <button onClick={getResponse}>Ask me</button>}
+          {error && <button onClick={clear}>Clear</button>}
+        </div>
+        {error && <p>{error}</p>}
+        <div className="search-result">
+          {/* {chatHistory.map((chatItem, idx) => <div key={idx}>
+            <p className="answer">{chatItem.role} : {chatItem.parts}</p>
+          </div>)} */}
+          <p className="answer">{value} : {ans}</p>
+        </div>
+      </section>
     </div>
   );
 }
 
 export default App;
+
+// run the react app frontent in a separate terminal from that of server.js by: npm run start:frontend
